d61d04f4e4ae204a75854b4f91d61132e484bbef
Harsh <harsh20221@gmail.com>
2024-07-11 13:21:11 +0530

    Great_places IOS

---
+++ lib/screens/places_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:greatplaces/models/place.dart';
+
+class PlaceListScreen extends StatelessWidget {
+  const PlaceListScreen({super.key, required this.places});
+  final List<Place> places;
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return const Center(child: Text('No Places Saved Yet Please Enter Some'));
+    } else
+      return ListView.builder(
+          itemBuilder: (ctx, index) => ListTile(
+              title: Text(places[index].title,
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground))));
+  }
+}
