ef0914f9242997020c7d4574f0a4393019b8c24a
Harsh <harsh20221@gmail.com>
2024-07-11 22:39:51 +0530

    More Changes

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget{  ////#Tip-- To create a Stateful Widget first Create A stateless Widget then use the Bulb Icon to Convert 
+///# to a Stateful Widget
+  const AddPlaceScreen ({super.key});
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  Widget build(BuildContext context ){
+    return AppBar(title: Text('Masai'),);}
+}
